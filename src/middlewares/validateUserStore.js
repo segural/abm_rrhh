31e0dbde0967db43f6aa2af25b7489d9826d565f
efesalinas <efesalinas@hotmail.com>
2023-01-31 19:39:21 -0300

    ABM usuarios internos 1

--- src/middlewares/validateUserStore.js
+++ src/middlewares/validateUserStore.js
@@ -19,6 +19,9 @@ let validateUserStore = [
     check("password")
         .isLength({min: 8}).withMessage("La contraseña debe tener entre 8 y 15 caracteres").bail()
         .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/).withMessage("La contraseña debe incluir al menos una minúscula, una mayúscula, un número y un caracter especial"),
+
+    check("roles")
+        .notEmpty().withMessage("Debes seleccionar al menos un rol").bail(),
 ];
 
 module.exports = validateUserStore;
